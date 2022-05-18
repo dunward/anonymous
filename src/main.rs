754eb959fd00847d83b8b4ebeba2c8ac888b397a
Min Song <min.song@bagelcode.com>
2022-05-19 00:29:43 +0900

    Draw red triangle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+#[macro_use]
 extern crate glium;
 
-use glium::glutin;
+use glium::{glutin, Surface};
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
 
 fn main() {
     let mut events_loop = glutin::event_loop::EventLoop::new();
@@ -9,6 +17,32 @@ fn main() {
                 .with_title("Anonymous");
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.5] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+        out vec4 color;
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
     
     events_loop.run(move |ev, _, control_flow| {
         let next_frame_time = std::time::Instant::now() +
@@ -24,5 +58,11 @@ fn main() {
             },
             _ => (),
         }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+                    &Default::default()).unwrap();
+        target.finish().unwrap();
     });
 }
