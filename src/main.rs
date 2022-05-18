d8151def1a833e2a54f4c387dd089b61b816131f
Min Song <min.song@bagelcode.com>
2022-05-19 00:02:55 +0900

    Create OpenGL window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate glium;
+
+use glium::glutin;
+
 fn main() {
-    println!("Hello, world!");
+    let mut events_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new()
+                .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0))
+                .with_title("Anonymous");
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &events_loop).unwrap();
+    
+    events_loop.run(move |ev, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
 }
